a4048cc30e5d6ba163c5f6c70a7c025780d58508
Nirali <nshah1824@gmail.com>
2017-10-09 15:49:00 +0000

    Update Test

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   get 'my_pages/home'
-
   get 'my_pages/help'
-
   get 'my_pages/about'
-
+ 
   resources :microposts
   resources :users
-  #root 'users#index'
-  root 'my_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'microposts#index'
 end
+
